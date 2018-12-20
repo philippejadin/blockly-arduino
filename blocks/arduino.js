9aded33bab53fef76896e15a5f9bb06c9819b718
Philippe Jadin <philippe.jadin@gmail.com>
2018-12-20 12:14:31 +0100

    SDS011 wip

--- blocks/arduino.js
+++ blocks/arduino.js
@@ -1278,19 +1278,40 @@ Blockly.Blocks["grove_ldr"] = {
 
 
 
-Blockly.Blocks['sds011'] = { init: function() {
-  this.appendDummyInput().appendField("Configurer le capteur de particules fines SDS011");
+Blockly.Blocks['sds011'] = {
+  init: function() {
+    this.appendDummyInput().appendField("Configurer le capteur de particules fines SDS011");
     this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("RX pin :")
-        .appendField(new Blockly.FieldNumber("1"), "RX");
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("RX pin :")
+      .appendField(new Blockly.FieldNumber("1"), "RX");
     this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("TX pin :")
-        .appendField(new Blockly.FieldNumber("2"), "TX");
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("TX pin :")
+      .appendField(new Blockly.FieldNumber("2"), "TX");
     this.setInputsInline(false);
-    this.setColour(315);
+    this.setColour("#00929F");
+    this.setTooltip('Ce bloc sert à initialiser le capteur de particules fines. Spécifiez les bonnes pin de connexion sur l\'arduino');
+    this.setHelpUrl('https://github.com/lewapek/sds-dust-sensors-arduino-library');
+  }
+};
+
+
+
+
+Blockly.Blocks['sds011_read'] = {
+  init: function() {
+    this.appendDummyInput()
+      .appendField("Quantité de particules fines");
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown([
+        ["pm2.5", "25"],
+        ["pm10", "10"]
+      ]), "pmtype");
+    this.setInputsInline(false);
+    this.setOutput(true);
+    this.setColour("#00929f");
     this.setTooltip('ce bloc sert à...');
-    this.setHelpUrl('http://www.mon-club-elec.fr/pmwiki_reference_arduino/pmwiki.php?n=Main.ReferenceMaxi');
+    this.setHelpUrl('https://github.com/lewapek/sds-dust-sensors-arduino-library');
   }
 };
--- generators/arduino.js
+++ generators/arduino.js
@@ -675,8 +675,29 @@ Blockly.Arduino['sds011'] = function(block) {
   Blockly.Arduino.includes_['sds011'] = '#include "SdsDustSensor.h"';
   //Blockly.Arduino.variables_['sds011'] = 'placer ici le code de mes variables';
   Blockly.Arduino.definitions_['sds011'] = 'SdsDustSensor sds(' + rx + ', ' + tx + ');';
-  //Blockly.Arduino.userFunctions_['sds011'] = 'placer ici le code de mes fonctions';
-  Blockly.Arduino.setups_['sds011']='sds.begin(); \n  sds.setActiveReportingMode();';
+  Blockly.Arduino.userFunctions_['sds011'] = `float sds011Read(int type)
+{
+  sds.queryPm();
+  if (pm.isOk()) {
+    if (type == 10)
+    {
+      return pm.pm10;
+    }
+    if (type == 25)
+    {
+      return pm.pm25;
+    }
+  }
+  return 0;
+}`;
+  Blockly.Arduino.setups_['sds011'] = 'sds.begin(); \n  sds.setActiveReportingMode();';
 
   return '';
+};
+
+
+Blockly.Arduino['sds011_read'] = function(block) {
+  var pmtype = block.getFieldValue('pmtype');
+  var code = "sds011Read(" + pmtype + ")";
+  return [code, Blockly.Arduino.ORDER_ATOMIC]
 };
